36c41e98573dcc0d6d9b5fb24f40eb2201dbdf15
DK Lee <origoni@daum.net>
2015-12-21 01:59:45 +0900

    Update AccountDao.java

--- src/main/java/org/springframework/social/showcase/account/AccountDao.java
+++ src/main/java/org/springframework/social/showcase/account/AccountDao.java
@@ -5,6 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface AccountDao extends CrudRepository<Account, Integer> {
 
 	Account findByUserId(String userId);
-
-//	Account findByUsername(String username);
 }
